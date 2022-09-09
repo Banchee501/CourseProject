2fddcd3c1898b8e9ec52ff47f0ef9b2cecc717ef
Banchee501 <rosstactical373@gmail.com>
2022-09-09 17:53:42 +0300

    delete old slides function

--- js/script.js
+++ js/script.js
@@ -385,38 +385,4 @@ document.addEventListener("DOMContentLoaded", () => {
             current.textContent = slideIndex;
         }
     });
-
-    // showSlides(slideIndex);
-
-    // function showSlides(n) {
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     }
-
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     }
-
-    //     slides.forEach(item => item.style.display = 'none');
-
-    //     slides[slideIndex - 1].style.display = 'block';
-
-    //     if (slides.length < 10) {
-    //         current.textContent = `0${slideIndex}`;
-    //     } else {
-    //         current.textContent = slideIndex;
-    //     }
-    // }
-
-    // function plusSlides(n) {
-    //     showSlides(slideIndex += n);
-    // }
-
-    // prev.addEventListener('click', () => {
-    //     plusSlides(-1);
-    // });
-
-    // next.addEventListener('click', () => {
-    //     plusSlides(1);
-    // });
 });
